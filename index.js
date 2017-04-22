77243d136544f72aaf2ff751bf1f288e06732df4
anajera1 <najera.anamaria@gmail.com>
2017-04-22 14:01:13 -0400

    added nav bar and css

--- index.js
+++ index.js
@@ -18,5 +18,18 @@ app.listen(app.get('port'), function() {
 });
 
 
+app.get('/about', function(request, response) {
+  response.render('pages/about');
+});
+
+app.get('/projects', function(request, response) {
+  response.render('pages/projects');
+});
+
+app.get('/contact', function(request, response) {
+  response.render('pages/contact');
+});
+
+
 // This file is what handles incoming requests and
 // serves files to the browser, or executes server-side code
